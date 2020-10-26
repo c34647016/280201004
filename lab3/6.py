4a239db503eaf8477a9cd788f0096342d9c03adf
c34647016 <enisozgun@std.iyte.edu.tr>
2020-10-26 09:20:43 +0000

    g

--- lab3/6.py
+++ lab3/6.py
@@ -1,6 +1,7 @@
-a=eval(input("Please enter parameters of a quadratic equation: "))
-b=eval(input("Please enter parameters of a quadratic equation: "))
-c=eval(input("Please enter parameters of a quadratic equation: "))
+print("In a quadratic equation represented as ax^2+bx+c;")
+a=eval(input("Please enter parameter a of a quadratic equation: "))
+b=eval(input("Please enter parameter b of a quadratic equation: "))
+c=eval(input("Please enter parameter c of a quadratic equation: "))
 delta=b**2-4*a*c
 if delta > 0 :
   print("There are two real roots.")
