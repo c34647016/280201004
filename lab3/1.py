a9b36379036a8437ada9e86f5ec26e479e5d9b5d
c34647016 <enisozgun@std.iyte.edu.tr>
2020-10-26 08:26:20 +0000

    absolute value

--- lab3/1.py
+++ lab3/1.py
@@ -1,9 +1,5 @@
-#If I leave my house at 6:52 am and run 1 mile at an easy pace (8 minutes per mile), then 3 miles at tempo (6 minutes per mile)and 2 mile at easy pace again, what time do I get home for breakfast?
-
-left_house = 412
-easy = 8
-tempo = 6 
-gone= 3*easy+3*tempo
-oclock=((left_house+gone)//60)
-minutes=left_house+gone-oclock*60
-print(str(oclock) + ":" + str(minutes))
+number=input("Please enter a number:")
+if eval(number) <= 0 :
+  print((-1)*eval(number))
+else :
+    print(number)
