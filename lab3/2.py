f785876081633e0e1386d1c80ff8dfab20f01dfc
c34647016 <enisozgun@std.iyte.edu.tr>
2020-10-26 08:41:55 +0000

    no min

--- lab3/2.py
+++ lab3/2.py
@@ -1,5 +1,9 @@
 n1=eval(input("Please enter first number: "))
 n2=eval(input("Please enter second number: "))
 n3=eval(input("Please enter third number: "))
-numbers=[n1, n2, n3]
-print(min(numbers))
+if n1 <= n2 and n1 <= n3:
+  print(n1)
+elif n2 <= n1 and n2 <= n3 :
+  print(n2)
+else :
+  print(n3)
