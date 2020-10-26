d8d9cb6ec78f2df9eb1c3a9dbe77997c1e7f1251
c34647016 <enisozgun@std.iyte.edu.tr>
2020-10-26 06:12:52 +0000

    left

--- lab3/1.py
+++ lab3/1.py
@@ -1,9 +1,9 @@
 #If I leave my house at 6:52 am and run 1 mile at an easy pace (8 minutes per mile), then 3 miles at tempo (6 minutes per mile)and 2 mile at easy pace again, what time do I get home for breakfast?
 
 left_house = 412
-easy_pace = 8
+easy = 8
 tempo = 6 
-total= 3*easy_pace+3*tempo
-oclock=((left_house+total)//60)
-minutes=left_house+total-oclock*60
-print(oclock,":",minutes)
+gone= 3*easy+3*tempo
+oclock=((left_house+gone)//60)
+minutes=left_house+gone-oclock*60
+print(str(oclock) + ":" + str(minutes))
