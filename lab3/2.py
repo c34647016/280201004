d6e041d93bd366d6bf6784dfddfe45aedf10712e
c34647016 <enisozgun@std.iyte.edu.tr>
2020-10-26 08:39:34 +0000

    numb

---
+++ lab3/2.py
@@ -0,0 +1,5 @@
+n1=eval(input("Please enter first number: "))
+n2=eval(input("Please enter second number: "))
+n3=eval(input("Please enter third number: "))
+numbers=[n1, n2, n3]
+print(min(numbers))
