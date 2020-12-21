7384a3f57ce47a8203c1886949ab2e86ffe9e77c
c34647016 <enisozgun@std.iyte.edu.tr>
2020-12-21 09:03:39 +0000

    se

--- lab9/2.py
+++ lab9/2.py
@@ -1,11 +1,10 @@
 def reverse(ylist):
-  nelist = []
-  while len(ylist) > 0:
-    nelist.append(ylist[-1])
-    ylist.remove(ylist[-1])
-  print(nelist)
+  if len(ylist) == 0:
+    return []
+  else:
+    return [ylist[-1]] + reverse(ylist[:-1])
 
 
 nums = [1, 2, 3, 4]
-reverse(nums)
-  
+a = reverse(nums)
+print(a)
